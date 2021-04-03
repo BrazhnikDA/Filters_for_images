50df69a7cd8e1619212669781f5a980b1195b3b9
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:13:14 +0300

    Фильтр сепия

---
+++ ComputerGrapgics_firstLab/allFilters/PointsFilters/SepiaFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ComputerGraphics_firstLab.allFilters.PointsFilters
+{
+    class SepiaFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            double k = 30;
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            int intensity = (int)(0.299 * sourceColor.R + 0.587 * sourceColor.G + 0.114 * sourceColor.B);
+
+            return Color.FromArgb(
+                                   Clamp((int)(intensity + 2 * k), 0, 255),
+                                   Clamp((int)(intensity + 0.5 * k),0,255),
+                                   Clamp((int)(intensity - 1 * k),0,255)
+                                   );
+        }
+    }
+}
