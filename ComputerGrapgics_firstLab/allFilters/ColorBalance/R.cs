27b47cd98d85d996b6adc04f8a15fb556cd032d4
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:09:46 +0300

    Цветовой баланс, выделения канала цвета

---
+++ ComputerGrapgics_firstLab/allFilters/ColorBalance/R.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace ComputerGraphics_firstLab.allFilters.ColorBalance
+{
+    class R : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+
+            return Color.FromArgb(
+                                   Clamp(255, 0, 255),
+                                   Clamp((sourceColor.G), 0, 255),
+                                   Clamp((sourceColor.B), 0, 255)
+                                   );
+        }
+    }
+}
