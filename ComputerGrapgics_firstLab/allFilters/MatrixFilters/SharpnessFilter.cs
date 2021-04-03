bf9812c588d999b7a0ca5b2979b64a7383b616ba
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:15:18 +0300

    Фильтр резкости

---
+++ ComputerGrapgics_firstLab/allFilters/MatrixFilters/SharpnessFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace ComputerGraphics_firstLab.allFilters.MatrixFilters
+{
+    class SharpnessFilter : MatrixFilter
+    {
+        public SharpnessFilter()
+        {
+            int sizeX = 3;
+            int sizeY = 3;
+            kernel = new float[sizeX, sizeY];
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    kernel[i, j] = -1;
+                }
+            }
+            kernel[1,1] = 9;
+        }
+    }
+}
