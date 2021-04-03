74ab820c958733ee1a55a0ab2ecaae6966d982b3
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:12:07 +0300

    Фильтр инверсия

---
+++ ComputerGrapgics_firstLab/allFilters/PointsFilters/InvertFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace ComputerGraphics_firstLab.allFilters.PointsFilters
+{
+    class InvertFilter : Filters
+    {
+
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+
+            return Color.FromArgb(
+                                  255 - sourceColor.R,
+                                  255 - sourceColor.G,
+                                  255 - sourceColor.B);
+        }
+    }
+}
