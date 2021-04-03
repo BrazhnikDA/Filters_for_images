34ce71f567f23a0af9929de4142bb6c0f4b40a52
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:08:47 +0300

    Форма для задания размера матрицы мат. морфологии

---
+++ ComputerGrapgics_firstLab/SettingsMathMorphology.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerGraphics_firstLab
+{
+    public partial class SettingsMathMorphology : Form
+    {
+        int size = 5;
+
+        public SettingsMathMorphology()
+        {
+            InitializeComponent();
+            textBox1.Text = size.ToString();
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                size = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                PrintError();
+            }
+
+            if((size < 2) || (size % 2 == 0))
+            {
+                PrintError();
+            }
+
+            Close();
+        }
+
+        private void PrintError()
+        {
+            MessageBox.Show("Введено неверное значение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
