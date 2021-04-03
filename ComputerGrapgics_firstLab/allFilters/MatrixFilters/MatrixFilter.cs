3bb1c7c0ad03afd4b581027c089ee3689d8732b5
M1ND228 <dimity.brazhnik@yandex.ru>
2021-04-03 17:10:10 +0300

    Класс для матричных фильтров

---
+++ ComputerGrapgics_firstLab/allFilters/MatrixFilters/MatrixFilter.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace ComputerGraphics_firstLab.allFilters
+{
+    class MatrixFilter : Filters
+    {
+        protected float[,] kernel = null;
+        protected MatrixFilter() { }
+        public MatrixFilter(float[,] kernel)
+        {
+            this.kernel = kernel;
+        }
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int radiusX = kernel.GetLength(0) / 2;
+            int radiusY = kernel.GetLength(1) / 2;
+
+            float resultR = 0;
+            float resultG = 0;
+            float resultB = 0;
+            for(int l = -radiusY; l <= radiusY; l++)
+            {
+                for(int k = -radiusX; k <= radiusX; k++)
+                {
+                    int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+                    int idY = Clamp(y + l, 0, sourceImage.Height - 1);
+
+                    Color neighborColor = sourceImage.GetPixel(idX, idY);
+                    resultR += neighborColor.R * kernel[k + radiusX, l + radiusY];
+                    resultG += neighborColor.G * kernel[k + radiusX, l + radiusY];
+                    resultB += neighborColor.B * kernel[k + radiusX, l + radiusY];
+                }
+            }
+            return Color.FromArgb(
+                Clamp((int)resultR,0,255),
+                Clamp((int)resultG,0,255),
+                Clamp((int)resultB,0,255)
+                );
+        }
+    }
+}
